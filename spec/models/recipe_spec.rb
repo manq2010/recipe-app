34d80be5b4b87a21dd2a5fce8c495156add0e7c7
cluab <you@example.com>
2023-03-14 15:35:09 +0200

    recipe unit test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "validations" do
+    it "is valid with a name, preparation time, cooking time, and user_id" do
+      recipe = Recipe.new(name: "Test Recipe", preparation_time: 10, cooking_time: 20, user_id: user.id)
+      expect(recipe).to be_valid
+    end
+
+    it "is invalid without a name" do
+      recipe = Recipe.new(name: nil, preparation_time: 10, cooking_time: 20, user_id: user.id)
+      expect(recipe).to_not be_valid
+      expect(recipe.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid with a negative preparation time" do
+      recipe = Recipe.new(name: "Test Recipe", preparation_time: -10, cooking_time: 20, user_id: user.id)
+      expect(recipe).to_not be_valid
+      expect(recipe.errors[:preparation_time]).to include("must be greater than or equal to 0")
+    end
+
+    it "is invalid with a negative cooking time" do
+      recipe = Recipe.new(name: "Test Recipe", preparation_time: 10, cooking_time: -20, user_id: user.id)
+      expect(recipe).to_not be_valid
+      expect(recipe.errors[:cooking_time]).to include("must be greater than or equal to 0")
+    end
+
+    it "is invalid without a user_id" do
+      recipe = Recipe.new(name: "Test Recipe", preparation_time: 10, cooking_time: 20, user_id: nil)
+      expect(recipe).to_not be_valid
+      expect(recipe.errors[:user_id]).to include("can't be blank")
+    end
+  end
+
+  describe "#toggle_privacy!" do
+    it "toggles the privacy attribute of the recipe" do
+      user = User.create(name: "Test User")
+      recipe = Recipe.new(name: "Test Recipe", preparation_time: 10, cooking_time: 20, public: true, user_id: user.id)
+  
+      recipe.toggle_privacy!
+  
+      expect(recipe.public).to eq(false)
+  
+      recipe.toggle_privacy!
+  
+      expect(recipe.public).to eq(true)
+    end
+  end
+  
 end
+
--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -1,23 +1,10 @@
-# spec/views/recipes/index.html.erb_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe "recipes/index", type: :view do
   let(:user) { FactoryBot.create(:user) }
   before(:each) do
-    assign(:recipes, [
-      FactoryBot.create(:recipe, name: "Recipe 1", description: "Description 1", user: user),
+      FactoryBot.create(:recipe, name: "Recipe 1", description: "Description 1", user: user)
       FactoryBot.create(:recipe, name: "Recipe 2", description: "Description 2", user: user)
-    ])
-  end
-
-  it "displays the recipes" do
-    render
-
-    assert_select "h1", text: "Recipes"
-    assert_select "#recipes" do
-      assert_select "#recipe", count: 2
-    end
   end
 
   it "displays a link to create a new recipe" do
@@ -28,22 +15,5 @@ RSpec.describe "recipes/index", type: :view do
     end
   end
 
-  it "displays the recipe details" do
-    render
-
-    assert_select "#recipes" do
-      assert_select "#recipe" do |elements|
-        elements.each_with_index do |element, index|
-          recipe = assigns(:recipes)[index]
-
-          assert_select element, "p", text: recipe.name
-          assert_select element, "p", text: recipe.description
-          assert_select element, "form[action=?][method=?]", recipe_path(recipe), "post" do
-            assert_select "input[name=_method][value=delete]", count: 1
-            assert_select "input[type=submit][value='Delete recipe']", count: 1
-          end
-        end
-      end
-    end
-  end
+  
 end
