0ef0379f26d0f3a2bbd3b7c18e34820b00de5ef8
manq2010 <manq2010@gmail.com>
2023-03-14 16:13:15 +0200

    Add and style food page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,12 +21,13 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @food = current_user.foods.new(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
+        # format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
+        redirect_to foods_path
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @food.errors, status: :unprocessable_entity }
