bc82fe3b56b4cd417af4d626ae2b76ebbfcc4da8
cluab <you@example.com>
2023-03-14 15:34:47 +0200

    recipe Integration tests

--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -1,34 +1,49 @@
+# spec/views/recipes/index.html.erb_spec.rb
+
 require 'rails_helper'
 
-RSpec.describe 'recipes/index', type: :view do
+RSpec.describe "recipes/index", type: :view do
+  let(:user) { FactoryBot.create(:user) }
   before(:each) do
     assign(:recipes, [
-             Recipe.create!(
-               name: 'Name',
-               preparation_time: 2,
-               cooking_time: 3,
-               description: 'Description',
-               public: false,
-               user: nil
-             ),
-             Recipe.create!(
-               name: 'Name',
-               preparation_time: 2,
-               cooking_time: 3,
-               description: 'Description',
-               public: false,
-               user: nil
-             )
-           ])
+      FactoryBot.create(:recipe, name: "Recipe 1", description: "Description 1", user: user),
+      FactoryBot.create(:recipe, name: "Recipe 2", description: "Description 2", user: user)
+    ])
   end
 
-  it 'renders a list of recipes' do
+  it "displays the recipes" do
     render
-    assert_select 'tr>td', text: 'Name'.to_s, count: 2
-    assert_select 'tr>td', text: 2.to_s, count: 2
-    assert_select 'tr>td', text: 3.to_s, count: 2
-    assert_select 'tr>td', text: 'Description'.to_s, count: 2
-    assert_select 'tr>td', text: false.to_s, count: 2
-    assert_select 'tr>td', text: nil.to_s, count: 2
+
+    assert_select "h1", text: "Recipes"
+    assert_select "#recipes" do
+      assert_select "#recipe", count: 2
+    end
+  end
+
+  it "displays a link to create a new recipe" do
+    render
+
+    assert_select ".new-recipe-link-div" do
+      assert_select "a.new-recipe-link[href=?]", new_recipe_path, text: "New recipe"
+    end
+  end
+
+  it "displays the recipe details" do
+    render
+
+    assert_select "#recipes" do
+      assert_select "#recipe" do |elements|
+        elements.each_with_index do |element, index|
+          recipe = assigns(:recipes)[index]
+
+          assert_select element, "p", text: recipe.name
+          assert_select element, "p", text: recipe.description
+          assert_select element, "form[action=?][method=?]", recipe_path(recipe), "post" do
+            assert_select "input[name=_method][value=delete]", count: 1
+            assert_select "input[type=submit][value='Delete recipe']", count: 1
+          end
+        end
+      end
+    end
   end
 end
