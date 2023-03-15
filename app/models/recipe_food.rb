395207092f1a05e60a0a3423075fdfafceddc7ee
cluab <you@example.com>
2023-03-15 18:32:39 +0200

    RECIPE_FOOD MODEL UNIT TESTS

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,4 +2,8 @@ class RecipeFood < ApplicationRecord
   belongs_to :user
   belongs_to :food
   belongs_to :recipe
+
+  validates :user, presence: true
+  validates :food, presence: true
+  validates :recipe, presence: true
 end
