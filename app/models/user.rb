d027f4fdaf8a75f89a57eedaf2de11b21e7120aa
cluab <you@example.com>
2023-03-15 13:11:16 +0200

    apply authorization on recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :recipes, foreign_key: 'user_id'
   has_many :recipe_foods, foreign_key: 'user_id'
   validates :name, presence: true
+
+
+  def admin?
+    role == 'admin'
+  end
 end
