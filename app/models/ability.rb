8e702b99ff3cf4f0312318c80d92ecada5f7d2a5
cluab <you@example.com>
2023-03-15 13:10:44 +0200

    ADD CANCAN GEM

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    # check if user is admin and give admin full privellage
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, Recipe, user_id: user.id
+      can :manage, RecipeFood, user_id: user.id
+    end
+  end
+end
