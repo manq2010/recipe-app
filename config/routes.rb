302ca266c7bfe0f198f67890319a5a0f033b43e9
cluab <you@example.com>
2023-03-15 12:59:02 +0200

    ADD PUBLIC PAGE

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  
-  
   devise_for :users, path: '', path_names: {
     sign_in: 'login', sign_out: 'logout',
     password: 'secret', confirmation: 'verification',
@@ -10,9 +8,10 @@ Rails.application.routes.draw do
   resources :recipes, only: [:index, :show, :new, :edit, :create, :destroy] do
     resources :recipe_foods, only: [:create, :new, :destroy]
   end
+  resources :public_recipes, only: [:index]
   resources :users
   resources :foods, only: [:index, :show, :new, :edit,:create, :destroy]
-  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   put '/recipes/:id/toggle_privacy', to: 'recipes#toggle_privacy', as: 'toggle_recipe_privacy'
   # Defines the root path route ("/")
