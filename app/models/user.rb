26d5dd19d0e8532f001b3cd8741687a9f397177e
manq2010 <manq2010@gmail.com>
2023-03-16 11:16:01 +0200

    Add Food integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :foods, foreign_key: 'user_id'
   has_many :recipes, foreign_key: 'user_id'
   has_many :recipe_foods, foreign_key: 'user_id'
-  validates :name, presence: true
+  validates :name, :role, presence: true
 
   def admin?
     role == 'admin'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { 'MyString' }
     role { 'MyString' }
+    email { 'My@String.com' }
+    password { 'MyString' }
   end
 end
